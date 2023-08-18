28c6a0e16c508cb5e03d7ccd88ec2f37b7b87cf8
Tiago <tiago@pop-os.localdomain>
2023-08-18 00:11:29 -0300

    gitIgnore-deploysh

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:Ryrden/SCC-0560-Caaso-Eletroniks.git main:gh-pages
+ git push -f git@github.com:Tiaguinho2002/Tiago.dev.git main:gh-pages
 
 cd -
